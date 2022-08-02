9b1bbfc681c2858fb482abe29d1ad91b05bbe0e6
Vladislav Putyrskiy <V.V.Putyrskiy@gmail.com>
2022-08-02 13:44:33 +0300

    Добавьте файлы проекта.

---
+++ 18.2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+
+int rabbit_stairjumper(int n)
+{
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+    return rabbit_stairjumper(n - 1) + rabbit_stairjumper(n - 2) + rabbit_stairjumper(n - 3);
+}
+
+int main() 
+{   
+    std::cout << rabbit_stairjumper(8);
+}
